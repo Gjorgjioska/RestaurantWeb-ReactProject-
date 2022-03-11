6880a54410ab3a8e858c4ec76adfb02698112086
Gjorgjioska <marina.gorgioska@gmail.com>
2022-03-11 20:17:04 +0100

    Building our Menu

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -5,17 +5,23 @@ import { SubHeading, MenuItem } from "../../components";
 import "./SpecialMenu.css";
 
 const SpecialMenu = () => (
-  <div className="app_specialMenu flex__center section__padding " id="menu">
+  <div className="app__specialMenu flex__center section__padding " id="menu">
     <div className="app__specialMenu-title">
       <SubHeading title="Menu That Fits You Palatte" />
       <h1 className="headtext__cormorant">Today's Special</h1>
     </div>
+
     <div className="app__specialMenu-menu">
-      <div className="app__specialMenu_wine flex__center">
-        <p className="app__specialMenu_menu_heading">Wine & Beer</p>
+      <div className="app__specialMenu-menu_wine flex__center">
+        <p className="app__specialMenu-menu_heading">Wine & Beer</p>
         <div className="app__specialMenu_menu_items">
           {data.wines.map((wine, index) => (
-            <p>{wine.title}</p>
+            <MenuItem
+              key={wine.title + index}
+              title={wine.title}
+              price={wine.price}
+              tags={wine.tags}
+            />
           ))}
         </div>
       </div>
@@ -24,16 +30,21 @@ const SpecialMenu = () => (
         <img src={images.menu} alt="menu-img" />
       </div>
 
-      <div className="app__specialMenu_cocktails flex__center">
-        <p className="app__specialMenu_menu_heading">Cocktails</p>
+      <div className="app__specialMenu-menu_cocktails flex__center">
+        <p className="app__specialMenu-menu_heading">Cocktails</p>
         <div className="app__specialMenu_menu_items">
           {data.cocktails.map((cocktail, index) => (
-            <p>{cocktail.title}</p>
+            <MenuItem
+              key={cocktail.title + index}
+              title={cocktail.title}
+              price={cocktail.price}
+              tags={cocktail.tags}
+            />
           ))}
         </div>
       </div>
     </div>
-    <div style={{ marginTop: "15px" }}>
+    <div style={{ marginTop: 15 }}>
       <button type="button" className="custom__button">
         View More
       </button>
