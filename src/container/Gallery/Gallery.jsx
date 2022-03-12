8ed2f828f3f0d09cbafa5c2b56f8baf0242c4c83
Gjorgjioska <marina.gorgioska@gmail.com>
2022-03-12 15:54:04 +0100

    Implementing style on GalleryImage page

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -9,7 +9,25 @@ import { images } from "../../constants";
 
 import "./Gallery.css";
 
+const galleryImages = [
+  images.gallery01,
+  images.gallery02,
+  images.gallery03,
+  images.gallery04,
+];
+
 const Gallery = () => {
+  const scrollRef = React.useRef(null);
+
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if (direction === "left ") {
+      current.scrollRight -= 300;
+    } else {
+      current.scrollLeft += 300;
+    }
+  };
   return (
     <div className="app__gallery flex__center">
       <div className="app__gallery-content">
@@ -22,6 +40,32 @@ const Gallery = () => {
         <button type="button" className="custom__button">
           View More
         </button>
+      </div>
+
+      <div className="app__gallery-images">
+        <div className="app_gallery-images-container" ref={scrollRef}>
+          {galleryImages.map((image, index) => (
+            <div
+              className="app_gallery-images_card flex__center"
+              key={`gallery_image-${index + 1}`}
+            >
+              <img src={image} alt="gallery" />
+              <BsInstagram className="gallery__image-icon" />
+            </div>
+          ))}
+        </div>
+
+        <div className="app__gallery-images_arrow">
+          <BsArrowLeftShort
+            className="gallery__arrow-icon"
+            onCanPlay={() => scroll("left")}
+          />
+
+          <BsArrowRightShort
+            className="gallery__arrow-icon"
+            onCanPlay={() => scroll("right")}
+          />
+        </div>
       </div>
     </div>
   );
