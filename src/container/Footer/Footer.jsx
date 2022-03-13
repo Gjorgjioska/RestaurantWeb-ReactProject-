f7e1614b768915f3592a4e289f9282ef391863f4
Gjorgjioska <marina.gorgioska@gmail.com>
2022-03-13 21:18:05 +0100

    My project is done

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -21,8 +21,8 @@ const Footer = () => (
       <div className="app__footer-links_logo">
         <img src={images.gericht} alt="footer_logo" />
         <p className="p__opensans">
-          &quot;The best way to find yourself is to lose yourself in the service
-          of others.&quot;
+          The best way to find yourself is to lose yourself in the service of
+          others
         </p>
         <img
           src={images.spoon}
